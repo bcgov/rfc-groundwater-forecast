16cf635b5c9e25033828a5aa98f25c915d312829
Jon <jon.goetz@gov.bc.ca>
2025-09-05 00:53:09 +0100

    add objectstore script

---
+++ 99_objectstore.R
@@ -0,0 +1,66 @@
+# Copyright 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+# ==============================================================================
+# Script name:      99_objectstore.R
+# ------------------------------------------------------------------------------
+# Script version:
+# 2025-09-04:       v1 - JG
+#
+# ------------------------------------------------------------------------------
+# Notes/Objectives:
+# script for putting forecasts onto objectstore
+#
+# ==============================================================================
+
+source("01_ConfigInputs.R")
+
+#create a new directory to save the figures too based on the date.
+
+
+library(aws.s3)
+
+figure_location <- "output/"
+output_path <- file.path(figure_location, "previous_forecasts", as.character(Sys.Date()))
+file_loc <- normalizePath(output_path)
+
+bucket <- "rfc-conditions/groundwater_forecast/outputs"
+region <- ""
+
+# Recursively list all files in the directory and subdirectories
+all_files <- list.files(file_loc, recursive = TRUE, full.names = TRUE)
+
+for (file_path in all_files) {
+
+  # Determine S3 object key (relative path from output_path)
+  relative_key <- sub(paste0(normalizePath(output_path), .Platform$file.sep), "", file_path, fixed = TRUE)
+
+  message("Uploading: ", relative_key)
+
+  if (relative_key == "Groundwater_Drought_Forecast_Map.html") {
+    put_object(file = file_path,
+               object = relative_key,
+               bucket = bucket,
+               region = region,
+               headers = list(`Content-Type` = "text/html"),
+               acl = "public-read")
+
+  } else {
+    put_object(file = file_path,
+               object = relative_key,
+               bucket = bucket,
+               region = region,
+               acl = "public-read")
+
+  }
+}
+
